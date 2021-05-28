78525c5d07b0f0ff01d7317face0a3aeac10839d
tylergneill <tyler.g.neill@gmail.com>
2021-05-28 00:02:43 -0400

    first LDAvis display working, putting toy online

--- flask_app.py
+++ flask_app.py
@@ -444,7 +444,7 @@ def pramana_NLP_main():
 def pramana_NLP_LDAvis():
 
 	import html
-	href = "https://raw.githubusercontent.com/tylergneill/skrutable_front_end/main/assets/ldavis_prepared_10.html"
+	href = "https://raw.githubusercontent.com/tylergneill/skrutable_front_end/main/assets/pramanaNLP/ldavis_prepared_10.html"
 	with open(href, 'r') as f_in:
 		topic_explorer_HTML = html.unescape(f_in.read())
 
