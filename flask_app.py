7c6416c32260969ecbcd218dc093035720a8772b
tylergneill <tyler.g.neill@gmail.com>
2022-12-16 20:56:52 -0500

    remove app.run command

--- flask_app.py
+++ flask_app.py
@@ -459,7 +459,6 @@ def prep_Apte_query(IAST_string):
 	dev_query = T.transliterate(IAST_string, from_scheme='IAST', to_scheme='DEV')
 	while dev_query[-1] == chr(0x94d): dev_query = dev_query[:-1] # remove final Devanagari virāma(s) (U+094d)
 	chicago_url = "https://dsal.uchicago.edu/cgi-bin/app/apte_query.py?qs=%s&searchhws=yes&matchtype=default" % dev_query
-	import pdb; pdb.set_trace()
 	return chicago_url
 
 @app.route('/ChicagoApteIAST') # hidden endpoint
@@ -478,7 +477,3 @@ def prep_split_output_for_Apte(split_text):
 			output_HTML += "<a href='%s' target='apteTab'>%s</a> " % (prep_Apte_query(word), word)
 	output_HTML += "</p>"
 	return output_HTML
-
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5000)
